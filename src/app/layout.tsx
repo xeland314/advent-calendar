f0e4b3b3fe15907e7997666e1c22d2ce6e4ca32d
Christopher Villamarín <alexpila.314@gmail.com>
2024-12-10 23:11:57 -0500

    fix SEO attributes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Tiny5 } from "next/font/google";
 import "./globals.css";
 import { Header } from "./components";
-import Head from "next/head";
 
 const tiny5 = Tiny5({
   variable: "--font-tiny5",
@@ -11,8 +10,34 @@ const tiny5 = Tiny5({
 });
 
 export const metadata: Metadata = {
-  title: "Advent Calendar",
-  description: "Advent Calendar",
+  title: "Calendario de Adviento",
+  description: "Calendario de Adviento",
+  authors: [
+    { name: "Christopher Villamarín", url: "https://xeland314.github.io/" },
+  ],
+  creator: "Christopher Villamarín",
+  icons: {
+    icon: "https://xeland314.github.io/advent-calendar/tapa.svg",
+    apple: "https://xeland314.github.io/advent-calendar/tapa.svg",
+  },
+  keywords: [
+    "calendario",
+    "adviento",
+    "navidad",
+    "regalos",
+    "sorpresas",
+    "diciembre",
+    "festividades",
+    "celebración",
+    "invierno",
+    "tradición",
+  ],
+  twitter: {
+    card: "summary_large_image",
+    site: "@site",
+    creator: "@creator",
+    images: ["https://xeland314.github.io/advent-calendar/tapa.svg"],
+  },
 };
 
 export default function RootLayout({
@@ -21,44 +46,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <Head>
-        <title>Calendario de Adviento</title>
-        <meta name="description" content="Calendario de Adviento" />
-        <meta
-          name="keywords"
-          content="calendario, adviento, navidad, regalos, sorpresas, diciembre, festividades, celebración, invierno, tradición"
-        />
-        <meta name="HandheldFriendly" content="True" />
-        <meta name="MobileOptimized" content="320" />
-        <meta name="referrer" content="no-referrer-when-downgrade" />
-        <meta property="og:title" content="Calendario de Adviento" />
-        <meta property="og:description" content="Calendario de Adviento" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content="https://xeland314.github.io/advent-calendar/"
-        />
-        <meta
-          property="og:image"
-          content="https://xeland314.github.io/advent-calendar/tapa.svg"
-        />
-        <meta property="og:site_name" content="Calendario de Adviento" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:image"
-          content="https://xeland314.github.io/advent-calendar/tapa.svg"
-        />
-        <meta name="twitter:title" content="Calendario de Adviento" />
-        <meta name="twitter:description" content="Calendario de Adviento" />
-        <meta itemProp="name" content="Calendario de Adviento" />
-        <meta itemProp="description" content="Calendario de Adviento" />
-        <link
-          rel="canonical"
-          href="https://xeland314.github.io/advent-calendar/"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <html lang="es">
       <body className={`tiny5-regular ${tiny5.variable} antialiased`}>
         <div className="w-full">
           <Header />
