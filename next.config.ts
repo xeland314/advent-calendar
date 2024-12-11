65c2ab22320e3938941e61a115a61c04ba391d64
Christopher Villamarín <alexpila.314@gmail.com>
2024-12-10 22:18:08 -0500

    update next js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "xeland314.github.io/advent-calendar",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  trailingSlash: true,
+  output: "export", // Habilita la exportación estática
+  basePath: "",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
